f45551efc61d8bc8f5a2d44cdb1fd2550d517b6b
webVueBlog <2979986590@qq.com>
2022-01-23 12:32:30 +0800

    SvgIcon

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import SvgIcon from 'vue-svgicon';
+import '@/assets/components';
+
+Vue.use(SvgIcon, {
+  tagName: 'svg-icon',
+  defaultWidth: '1em',
+  defaultHeight: '1em',
+});
 
 Vue.config.productionTip = false;
 
